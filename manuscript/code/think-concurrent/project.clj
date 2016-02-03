d98c2906b627be415ef4a7772ca2242f872c2bc3
Frederic Peschanski <not provided>
2016-02-03 23:51:17 +0100

    version bump

--- manuscript/code/think-concurrent/project.clj
+++ manuscript/code/think-concurrent/project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/fredokun/think-concurrent"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]]
+  :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
   :main ^:skip-aot think-concurrent.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
