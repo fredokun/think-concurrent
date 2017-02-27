0d1cbf35cdd73fd84c2d46d37316a265d29a7ade
Fredokun <fredokun@github.com>
2017-02-27 13:51:07 +0100

    use threads, go will come lateré

--- manuscript/code/think-concurrent/src/think_concurrent/pingpong.clj
+++ manuscript/code/think-concurrent/src/think_concurrent/pingpong.clj
@@ -1,38 +1,37 @@
 (ns think-concurrent.pingpong
   (:require [clojure.core.async
-             :refer [>!! >! <!! <! chan go]]))
+             :refer [>!! <!! chan thread]]))
 
 (defn pingpong
   [ch kind]
-  (go (loop []
-        (let [msg (<! ch)]
-          (print msg) (flush)
-          (>! ch kind)
-          (recur)))))
+  (loop []
+    (let [msg (<!! ch)]
+      (print msg) (flush)
+      (>!! ch kind)
+      (recur))))
 
 (defn run-pingpong
   []
   (let [ch (chan)]
-    (let [j (pingpong ch "Ping! ")
-          _ (pingpong ch "Pong! ")]
+    (let [j (thread (pingpong ch "Ping! "))
+          _ (thread (pingpong ch "Pong! "))]
       (>!! ch "Start! ") 
       (<!! j))))
 
-
 (defn pingpong-fuel
   [ch kind fuel]
-  (go (loop [level fuel]
-        (when (> level 0)
-          (let [msg (<! ch)]
-            (print msg) (flush)
-            (>! ch kind)
-            (recur (- level 1)))))))
+  (loop [level fuel]
+    (when (> level 0)
+      (let [msg (<!! ch)]
+        (print msg) (flush)
+        (>!! ch kind)
+        (recur (- level 1))))))
 
 (defn run-pingpong-fuel
   [fuel]
   (let [ch (chan)]
-    (let [j (pingpong-fuel ch "Ping! " fuel)
-          _ (pingpong-fuel ch "Pong! " (inc fuel))] ;; or (+ fuel 1) ?
+    (let [j (thread (pingpong-fuel ch "Ping! " fuel))
+          _ (thread (pingpong-fuel ch "Pong! " (inc fuel)))]
       (Thread/sleep 100) ;; non-determinism please !
       (>!! ch "Start! ") 
       (<!! j)
