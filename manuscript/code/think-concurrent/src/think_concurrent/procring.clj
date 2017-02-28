71ffb83533cd9301d03f80da0c3cf5ebc8f525c8
Fredokun <fredokun@github.com>
2017-02-28 16:10:15 +0100

    procring diagram

--- manuscript/code/think-concurrent/src/think_concurrent/procring.clj
+++ manuscript/code/think-concurrent/src/think_concurrent/procring.clj
@@ -1,7 +1,7 @@
 (ns think-concurrent.procring
   (:require [clojure.core.async
              :refer [>!! <!! >! <! chan thread go]]
-            [think-concurrent.utils
+            [think-concurrent.utils :as u
              :refer [log]]))
 
 (defn node-thread [id in out nb-turns leader?]
@@ -9,7 +9,9 @@
     (loop [turn 1]
       (when (<= turn nb-turns)
         (let [tok (<!! in)]
-          (log (str "[" id "] '" tok "' received at turn #" turn))
+          (log (str "[" id "]" (if leader?
+                                 "(leader)"
+                                 "") "'" tok "' received at turn #" turn))
           (when (or (not leader?)
                     (not= turn nb-turns))
             (>!! out tok)
@@ -24,10 +26,45 @@
                    (recur (inc id-node) out))
                  in))]
     ;; last process
+    (let [leader (node-thread nb-nodes exit init nb-turns true)]
+      ;; let's start
+      (>!! init :token)
+      (<!! leader))))
+
+
+(defn node-go [id in out nb-turns]
+  (go
+    (loop [turn 1]
+      (when (<= turn nb-turns)
+        (let [tok (<! in)]
+          (log (str "[" id "] '" tok "' received at turn #" turn))
+          (>! out tok)
+          (recur (inc turn)))))))
+
+(defn run-ring-go [nb-nodes nb-turns]
+  (let [init (chan)
+        exit (loop [id-node 1, in init]
+               (if (< id-node nb-nodes)
+                 (let [out (chan)]
+                   (node-go id-node in out nb-turns)
+                   (recur (inc id-node) out))
+                 in))]
+    ;; last process
     (let [lastproc (node-thread nb-nodes exit init nb-turns true)]
       ;; let's start
       (>!! init "token")
       (<!! lastproc))))
+
+
+;; timings
+(comment
+  (time (binding [u/*log-enabled* false]
+          (run-ring-thread 10000 100)))
+
+  (time (binding [u/*log-enabled* false]
+          (run-ring-go 10000 100))))
+
+
 
 
 
